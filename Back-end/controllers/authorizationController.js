00795172b218f65a4936bcf50837211fad34a9a6
reima852 <reimani@metropolia.fi>
2022-04-21 11:08:37 +0300

    Added a ton of dependencies

--- Back-end/controllers/authorizationController.js
+++ Back-end/controllers/authorizationController.js
@@ -1,1 +1,60 @@
 'use strict';
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const userModel = require('../models/userModel');
+const bcryptjs = require('bcryptjs');
+const {validationResult} = require("express-validator");
+
+
+const login = (req, res, next) => {
+  passport.authenticate('local', {session: false}, (error, user) => {
+    //In case of errors or missing user
+    if (error || !user) {
+      return res.status(400).json({
+        message: `Authentication failed due to bad credentials}`
+      });
+    }
+    //If no errors
+    req.login(user, {session: false}, (error) => {
+      if (error)  {
+        res.send(error);
+      }
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      return res.json({user, token});
+    });
+
+  })(req, res, next);
+};
+
+const userCreate_post = async (req, res, next) => {
+  // Extract the validation errors from a request.
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log('user create error', errors);
+    res.send(errors.array());
+  } else {
+    // bcrypted password
+    const cryptedPass = await bcryptjs.hash(req.body.password, 15);
+    //Account role by default is a normal user
+    const userRole = 1;
+
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: cryptedPass,
+      role: userRole
+    }
+    const result = await userModel.createUser(user, res);
+    if (result.insertId) {
+      res.json({ message: `User added`, user_id: result.insertId });
+    } else {
+      res.status(400).json({error: 'register error'});
+    }
+  }
+};
+
+module.exports = {
+  login,
+  userCreate_post,
+};
--- Back-end/controllers/userController.js
+++ Back-end/controllers/userController.js
@@ -1,1 +1,44 @@
 'use strict';
+
+// userController
+const userModel = require('../models/userModel');
+const {validationResult} = require("express-validator");
+
+
+//Authentication
+const checkToken = (req, res, next) => {
+  if (!req.user) {
+    next(new Error('token not valid'));
+  } else {
+    res.json({ user: req.user });
+  }
+};
+
+//Possibly for moderator to be able to see reported users
+const user_get_byId = async (req, res) => {
+  console.log('user get by id', req.params.id);
+  const userFilter = users.filter((item) => item.id == req.params.id);
+  res.json(userFilter[0] || {})
+};
+
+//For updating users
+const user_put = async (req, res) => {
+  console.log('user controller update body', req.body);
+  const newUser = req.body;
+  const updated = await userModel.updateUser(req.user, newUser, res);
+  res.json({message: `user updated: ${updated}.`});
+}
+
+//For deleting accounts
+const user_delete_byId = async (req, res) => {
+  console.log('User controller delete by id: ', req.params.id);
+  const del = await userModel.deleteUser(req.user, req.params.id, res);
+  res.json({message: `user deleted: ${del}.`});
+}
+
+module.exports = {
+  user_get_byId,
+  user_put,
+  user_delete_byId,
+  checkToken,
+};
