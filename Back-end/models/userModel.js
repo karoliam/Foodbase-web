569b3c947f94ba7f9f3585b971b1a8158dd7ee64
vanchess-git <72012822+vanchess-git@users.noreply.github.com>
2022-04-22 15:42:55 +0300

    database queries & changes in models

--- Back-end/models/userModel.js
+++ Back-end/models/userModel.js
@@ -9,7 +9,7 @@ const getUserLogin = async (params) => {
   try {
     console.log(params);
     const [rows] = await promisePool.execute(
-        'SELECT * FROM food_user WHERE email = ?;',
+        'SELECT * FROM user WHERE email = ?;',
         params);
     return rows;
   } catch (e) {
@@ -20,7 +20,7 @@ const getUserLogin = async (params) => {
 
 const getAllUsers = async (res) => {
   try {
-    const [rows] = await promisePool.query('SELECT * FROM food_user');
+    const [rows] = await promisePool.query('SELECT * FROM user');
     return rows;
   } catch (e) {
     console.error('userModel getAllUsers error', e.message);
@@ -31,7 +31,7 @@ const getAllUsers = async (res) => {
 //Possibly for moderator to be able to see flagged users
 const getUserById = async (id, res) => {
   try {
-    const [rows] = await promisePool.query('SELECT * FROM food_user WHERE user_id = ?', [id]);
+    const [rows] = await promisePool.query('SELECT * FROM user WHERE id = ?', [id]);
     return rows[0];
   } catch (e) {
     console.error('userModel getUserById error', e.message);
@@ -43,8 +43,11 @@ const getUserById = async (id, res) => {
 //For creating new users
 const createUser = async (user, res) => {
   try {
-    const [rows] = await promisePool.query('INSERT INTO food_user(name, email, password, role, preferences) VALUES (?,?,?,?)',
-        [user.name,user.email,user.password,user.role, user.preferences]);
+    // TODO preferences have to be saved in seprate table
+    // TODO so preferneces & user comes here in same package
+    // TODO and create rows on separate tables
+    const [rows] = await promisePool.query('INSERT INTO user(username, email, password, role) VALUES (?,?,?,?)',
+        [user.name,user.email,user.password,user.role]);
     console.log('user model insert: ', rows);
     return rows;
   } catch (e) {
@@ -58,13 +61,13 @@ const updateUser = async (user, newUser, res) => {
   try {
     if (user.role === 0) {
       //admin can update which user they ever want to.
-      const [rows] = await promisePool.query('UPDATE food_user SET name = ?, email = ?, password = ?, role = ? WHERE user_id=?',
+      const [rows] = await promisePool.query('UPDATE user SET username = ?, email = ?, password = ?, role = ? WHERE id=?',
           [newUser.name,newUser.email,newUser.passwd,newUser.role, newUser.user_id]);
       console.log('user model admin update: ', rows);
       return rows.affectedRows === 1;
     } else {
       //normal users can update only their own user except for role
-      const [rows] = await promisePool.query('UPDATE food_user SET name = ?, email = ?, password = ? WHERE user_id=? AND user_id=?',
+      const [rows] = await promisePool.query('UPDATE user SET username = ?, email = ?, password = ? WHERE id=? AND user_id=?',
           [newUser.name,newUser.email,newUser.passwd, newUser.user_id, user.user_id]);
       console.log('user model normal update: ', rows);
       return rows.affectedRows === 1;
@@ -80,12 +83,12 @@ const deleteUser = async (user, id, res) => {
   try {
     if (user.role === 0) {
       //Moderator can delete which user they ever want to.
-      const [rows] = await promisePool.query('DELETE from food_user WHERE user_id=?', [id]);
+      const [rows] = await promisePool.query('DELETE from user WHERE id=?', [id]);
       console.log('user model admin delete: ', rows);
       return rows.affectedRows === 1;
     } else {
       //normal users can delete only their own user
-      const [rows] = await promisePool.query('DELETE from food_user WHERE user_id=? AND user_id=?', [id, user.user_id]);
+      const [rows] = await promisePool.query('DELETE from user WHERE id=? AND id=?', [id, user.user_id]);
       console.log('user model normal delete: ', rows);
       return rows.affectedRows === 1;
     }
