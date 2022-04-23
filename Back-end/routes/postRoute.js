0a19a15bf9edcf3f5f17ba2aa1042f3180764f5d
karoliam <kmultas@gmail.com>
2022-04-23 20:21:43 +0300

    added filetype validation to postRoute

--- Back-end/routes/postRoute.js
+++ Back-end/routes/postRoute.js
@@ -4,17 +4,26 @@ const express = require('express');
 const postController = require('../controllers/postController');
 const router = express.Router();
 const multer  = require('multer');
-const upload = multer({dest: './uploads/'})
 const {body} = require('express-validator');
 
+
+const validateFileFormat = (req, file, cb) => {
+  const allowedMimetypes = ['image/png', 'image/jpeg', 'image/gif', 'image/jpg'];
+  if (allowedMimetypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({ dest: './uploads/', validateFileFormat });
 
 router.route('/')
 .get(postController.post_list_get)
 .post(upload.single('post'),
-    body('post-area').isLength({min: 1}),
-    body('expiration').isDate(),
-    body('title-edit').isLength({min:1}),
-    body('description-edit').isLength({min:1}),
+    body('post-area', 'Area must contain minimum 2 characters').isLength({min: 2}).escape(),
+    body('expiration', 'Not a valid date').isDate(),
+    body('title-edit', 'Title must contain minimum 2 characters').isLength({min:2}).escape(),
+    body('description-edit', 'Description must contain minimum 2 characters').isLength({min:2}).escape(),
     postController.post_posting)
 .put(postController.post_update_put);
 
